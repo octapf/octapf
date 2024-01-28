daf37784688ff19faf2e718de191e00643e41c47
octapf <frangipani.octavio@gmail.com>
2024-01-28 12:31:30 +0100

    add background effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import { Container, Hero, Arrow, Portals } from './components/'
 
 function App() {
 	return (
-		<Container type={'app-container'}>
-			<Hero />
-			<Arrow />
-			<Portals />
-		</Container>
+		<>
+			<Container type={'app-container'}>
+				<Hero />
+				<Arrow />
+				<Portals />
+			</Container>
+			<div className='bg'></div>
+			<div className='bg bg2'></div>
+			<div className='bg bg3'></div>
+		</>
 	)
 }
 
--- src/components/portals/Portals.tsx
+++ src/components/portals/Portals.tsx
@@ -12,86 +12,90 @@ import {
 
 export const Portals = () => {
 	return (
-		<Container type='flex-column-container'>
-			<h1
-				id='portals'
-				className={styles.title}
-			>
-				GET TO KNOW MY PROJECTS 🧪
-			</h1>
-			<Container type='flex-row-container'>
-				<Portal
-					propStyles={styles.frangipanidev}
-					src={'/cv'}
-					image={frangipaniDevLogo}
-					description='My online CV'
-				/>
+		<Container type={`flex-column-container`}>
+			<Container type={`${styles.section} flex-column-container`}>
+				<h1
+					id='portals'
+					className={styles.title}
+				>
+					GET TO KNOW MY PROJECTS 🧪
+				</h1>
+				<Container type={`flex-row-container`}>
+					<Portal
+						propStyles={styles.frangipanidev}
+						src={'/cv'}
+						image={frangipaniDevLogo}
+						description='My online CV'
+					/>
 
-				<Portal
-					propStyles={styles.frangipanidev}
-					image={frangipaniDevLogo}
-					src={'https://cocktail-app-frontend.vercel.app/'}
-					description='Cocktail Wiki'
-				/>
+					<Portal
+						propStyles={styles.frangipanidev}
+						image={frangipaniDevLogo}
+						src={'https://cocktail-app-frontend.vercel.app/'}
+						description='Cocktail Wiki'
+					/>
 
-				<Portal
-					propStyles={styles.lgv}
-					image={lgvLogo}
-					src={'https://lagaritaverde.com'}
-				/>
-				<Portal
-					propStyles={styles.lgv}
-					image={lgvLogo}
-					src={'https://ui-library.lagaritaverde.com/'}
-					description='UI Library App'
-				/>
-				<Portal
-					propStyles={styles.lgv}
-					image={lgvLogo}
-					src={'https://ticketscanner.lagaritaverde.com/'}
-					description='Ticket Scanner App'
-				/>
-				<Portal
-					propStyles={styles.ita}
-					image={itaLogo}
-					src={'https://dev.itawiki.eurecatacademy.org/'}
-					title='ITA Wiki'
-					description='Barcelona Activa'
-				/>
+					<Portal
+						propStyles={styles.lgv}
+						image={lgvLogo}
+						src={'https://lagaritaverde.com'}
+					/>
+					<Portal
+						propStyles={styles.lgv}
+						image={lgvLogo}
+						src={'https://ui-library.lagaritaverde.com/'}
+						description='UI Library App'
+					/>
+					<Portal
+						propStyles={styles.lgv}
+						image={lgvLogo}
+						src={'https://ticketscanner.lagaritaverde.com/'}
+						description='Ticket Scanner App'
+					/>
+					<Portal
+						propStyles={styles.ita}
+						image={itaLogo}
+						src={'https://dev.itawiki.eurecatacademy.org/'}
+						title='ITA Wiki'
+						description='Barcelona Activa'
+					/>
+				</Container>
 			</Container>
-			<h1
-				id='portals'
-				className={styles.title}
-			>
-				QR codes 📷
-			</h1>
-			<Container type={`${styles.multiportalContainer}`}>
-				<Portal
-					multiPortal={true}
-					propStyles={styles.qrcode}
-					image={qrcode_frangipani}
-					description='frangipani.dev'
-				/>
+			<Container type={`${styles.section2} flex-column-container`}>
+				<h1
+					id='portals'
+					className={styles.title}
+				>
+					QR codes 📷
+				</h1>
+				<Container type={`${styles.multiportalContainer}`}>
+					<Portal
+						multiPortal={true}
+						propStyles={styles.qrcode}
+						image={qrcode_frangipani}
+						description='frangipani.dev'
+					/>
 
-				<Portal
-					multiPortal={true}
-					propStyles={styles.qrcode}
-					image={qrcode_github_lgv}
-					description='La garita Verde - Github'
-				/>
+					<Portal
+						multiPortal={true}
+						propStyles={styles.qrcode}
+						image={qrcode_github_lgv}
+						description='La garita Verde - Github'
+					/>
 
-				<Portal
-					multiPortal={true}
-					propStyles={styles.qrcode}
-					image={qrcode_linkedin_octaviofrangipani}
-					description='Octavio Frangipani Linkedin'
-				/>
-				<Portal
-					multiPortal={true}
-					propStyles={styles.qrcode}
-					image={qrcode_github_octapf}
-					description='frangipani.dev - Github'
-				/>
+					<Portal
+						multiPortal={true}
+						propStyles={styles.qrcode}
+						image={qrcode_linkedin_octaviofrangipani}
+						description='Octavio Frangipani Linkedin'
+					/>
+					<Portal
+						multiPortal={true}
+						propStyles={styles.qrcode}
+						image={qrcode_github_octapf}
+						description='frangipani.dev - Github'
+					/>
+				</Container>
 			</Container>
 		</Container>
 	)
